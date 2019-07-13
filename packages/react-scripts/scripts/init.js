ce443bccb97b27170029b8245e93de1c2aaa2aa0
Shane Mann <shane.mann@gmail.com>
2019-07-13 14:01:05 -0500

    uvmetal-react-scripts

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -219,6 +219,11 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(chalk.red('VERY IMPORTANT:'));
+  console.log('Create a .env file at the root of your project with REACT_APP_EMPLOYEE_ID and REACT_APP_POSITION_ID');
+  console.log('  You can find these values in the company dashboard under application settings.');
+  console.log('  https://metal.uv/settings');
+  console.log();
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
